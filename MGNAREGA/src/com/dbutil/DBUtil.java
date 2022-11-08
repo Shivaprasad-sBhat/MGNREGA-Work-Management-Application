4f9bc96676e1439dc33294754070c2cd967689d7
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-11-08 22:51:01 +0530

    mgnarega

---
+++ MGNAREGA/src/com/dbutil/DBUtil.java
@@ -0,0 +1,40 @@
+package com.dbutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+
+
+	public static Connection provideConection() {
+		
+		Connection conn=null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		try {
+			String url="jdbc:mysql://localhost:3306/mgnareaga";
+			
+		conn = DriverManager.getConnection(url,"root","1234");
+		
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+}
